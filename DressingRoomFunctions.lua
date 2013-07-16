7c4aa8fab09860a03c00e2869ef3c55e2462657b
Rachael Alexanderson <raa-eruanna@users.noreply.github.com>
2013-07-16 01:12:04 +0000

    - Bump to v0.1 - Added arbitrary race function. This allows you to preview pretty much any model. It's kinda buggy, though, but most of these are Blizzard-related.

--- DressingRoomFunctions.lua
+++ DressingRoomFunctions.lua
@@ -16,27 +16,44 @@
 --    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 
-local frame = CreateFrame("FRAME", "DRFFrame");
-frame:RegisterEvent("PLAYER_ENTERING_WORLD");
+local function Noop() end
+
+local _backgroundList = {
+	[1] = "Human",
+	[2] = "Orc",
+	[3] = "Dwarf",
+	[4] = "NightElf",
+	[5] = "Scourge",
+	[6] = "Tauren",
+	[7] = "Gnome",
+	[8] = "Troll",
+	[9] = "Goblin",
+	[10] = "BloodElf",
+	[11] = "Draenei",
+	[22] = "Worgen",
+	[24] = "Pandaren"
+};
 
 DRF_Version = GetAddOnMetadata("DressingRoomFunctions","Version");
 
-DRF_button1 = CreateFrame("Button","UndressButton",DressUpFrame,"UIPanelButtonTemplate");
-DRF_button2 = CreateFrame("Button","TargetButton",DressUpFrame,"UIPanelButtonTemplate");
+local DRF_button1 = CreateFrame("Button","DRF_UndressButton",DressUpFrame,"UIPanelButtonTemplate");
+local DRF_button2 = CreateFrame("Button","DRF_TargetButton",DressUpFrame,"UIPanelButtonTemplate");
+local DRF_button3 = CreateFrame("Button","DRF_RaceButton",DressUpFrame,"UIPanelButtonTemplate");
+local DRF_menu1 = CreateFrame("FRAME","DRF_RaceMenu",DRF_button3,"UIDropDownMenuTemplate");
 
 
 DRF_button1:SetPoint("Center",DressUpFrame,"TopLeft",50,-421);
 DRF_button1:SetSize(70,22);
-DRF_button1.text = _G["UndressButton"];
+DRF_button1.text = _G["DRF_UndressButton"];
 DRF_button1.text:SetText("Undress");
 DRF_button1:SetScript("OnClick",function(self,event,arg1)
 	DressUpModel:Undress();
 	PlaySound("gsTitleOptionOK");
 end);
 
-DRF_button2:SetPoint("Center",UndressButton,"Center",62,0);
+DRF_button2:SetPoint("Center",DRF_UndressButton,"Center",62,0);
 DRF_button2:SetSize(60,22);
-DRF_button2.text = _G["TargetButton"];
+DRF_button2.text = _G["DRF_TargetButton"];
 DRF_button2.text:SetText("Target");
 DRF_button2:SetScript("OnClick",function(self,event,arg1)
 	local race, fileName = UnitRace("target");
@@ -52,6 +69,71 @@ DRF_button2:SetScript("OnClick",function(self,event,arg1)
 	PlaySound("gsTitleOptionOK");
 end);
 
+DRF_button3:SetPoint("Center",DRF_TargetButton,"Center",42,0);
+DRF_button3:SetSize(30,22);
+DRF_button3.text = _G["DRF_RaceButton"];
+DRF_button3.text:SetText("...");
+
+local function DRF_SetArbitraryRace(id,gender)
+	DressUpModel:SetCustomRace(id,gender);
+	SetDressUpBackground(DressUpFrame, _backgroundList[id]);
+
+	-- Puts a helmet on the character, to fix a bug using hidden helmets.
+	-- This chosen helmet is somewhat invisible, it's a holiday reward from
+	-- the midsummer fire festival.
+	DressUpModel:TryOn(23323);
+end
+
+local function DRF_menu1_OnClick(self, arg1, arg2, checked)
+	DRF_SetArbitraryRace(arg1,arg2);	
+end
+
+DRF_menu1:SetPoint("CENTER");
+--UIDropDownMenu_SetWidth(DRF_menu1, 200);
+--UIDropDownMenu_SetText(DRF_menu1, "Select Race/Gender:");
+UIDropDownMenu_Initialize(DRF_menu1, function(self, level, menuList)
+	local info = UIDropDownMenu_CreateInfo()
+	if level == 1 then
+		info.checked = false;
+		info.text = "Male";
+		info.menuList, info.hasArrow = 0, true;
+		UIDropDownMenu_AddButton(info, level);
+		info.text = "Female";
+		info.menuList, info.hasArrow = 1, true;
+		UIDropDownMenu_AddButton(info, level);
+	else
+		info.checked = false;
+		info.func = DRF_menu1_OnClick;
+		info.arg2 = menuList;
+		info.text, info.arg1 = "Human", 1;
+		UIDropDownMenu_AddButton(info, level);
+		info.text, info.arg1 = "Orc", 2;
+		UIDropDownMenu_AddButton(info, level);
+		info.text, info.arg1 = "Dwarf", 3;
+		UIDropDownMenu_AddButton(info, level);
+		info.text, info.arg1 = "Night Elf", 4;
+		UIDropDownMenu_AddButton(info, level);
+		info.text, info.arg1 = "Undead", 5;
+		UIDropDownMenu_AddButton(info, level);
+		info.text, info.arg1 = "Tauren", 6;
+		UIDropDownMenu_AddButton(info, level);
+		info.text, info.arg1 = "Gnome", 7;
+		UIDropDownMenu_AddButton(info, level);
+		info.text, info.arg1 = "Troll", 8;
+		UIDropDownMenu_AddButton(info, level);
+		info.text, info.arg1 = "Goblin", 9;
+		UIDropDownMenu_AddButton(info, level);
+		info.text, info.arg1 = "Blood Elf", 10;
+		UIDropDownMenu_AddButton(info, level);
+		info.text, info.arg1 = "Draenei", 11;
+		UIDropDownMenu_AddButton(info, level);
+		info.text, info.arg1 = "Worgen", 22;
+		UIDropDownMenu_AddButton(info, level);
+		info.text, info.arg1 = "Pandaren", 24;
+		UIDropDownMenu_AddButton(info, level);
+	end
+end, "MENU");
+
 DressUpFrameResetButton:SetScript("OnClick",function(self,event,arg1)
 	local race, fileName = UnitRace("player");
 
@@ -59,4 +141,9 @@ DressUpFrameResetButton:SetScript("OnClick",function(self,event,arg1)
 	DressUpModel:Dress();
 	SetDressUpBackground(DressUpFrame, fileName);
 	PlaySound("gsTitleOptionOK");
+end);
+
+
+DRF_button3:SetScript("OnClick",function(self,event,arg1)
+	ToggleDropDownMenu(1, nil, DRF_menu1, "cursor", 3, -3);
 end);
