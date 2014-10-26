7f2f4bf55b1a1c2b65e66c8af974aa603476a5e4
allikitten <allikitten@d4039ffd-a53f-40a8-b765-2a061f6c40f1>
2014-10-26 13:56:01 +0000

    ï»¿- Version bump - Fixed a line wrap typo

--- trunk/DressingRoomFunctions.lua
+++ trunk/DressingRoomFunctions.lua
@@ -1,4 +1,4 @@
-DRF_CoreVersion = "v1.3";
+DRF_CoreVersion = "v1.3.1";
 --
 --    Dressing Room Functions - Allows undress and target model for dressing room
 --    Copyright (C) 2014  Rachael Alexanderson
